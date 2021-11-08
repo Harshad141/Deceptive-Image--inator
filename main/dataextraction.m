691c33943665176ff3234e495850ee0ca2cc7098
Harshad Dhane <59340082+Harshad141@users.noreply.github.com>
2021-11-09 03:23:43 +0530

    Create dataextraction.m

---
+++ main/dataextraction.m
@@ -0,0 +1,31 @@
+clc;
+close all;
+clear all;
+E=imread('man.tiff');
+[R,C,Z]=size(E);
+H=imhist(E);
+[Height, Peak]=max(H);
+PixPeak=Peak-1;
+IR=uint8(zeros(R,C));
+ED=zeros(1,Height);%extracted data
+k=1;
+for x=1:R
+    for y=1:C
+        if E(x,y)==PixPeak
+            ED(1,K)=0;
+            k=k+1;
+        elseif E(x,y)==(PixPeak+1)
+            ED(1,k)=1;
+            k=k+1;
+        end
+        if E(x,y)<=PixPeak
+            IR(x,y)=E(x,y);
+        elseif E(x,y) > PixPeak
+            IR(x,y)=R(x,y);
+        end
+    end
+end
+imshow(IR);title('recovered image');
+        
+          
+            
